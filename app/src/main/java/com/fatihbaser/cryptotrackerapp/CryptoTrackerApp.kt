13877d9651540acdee77d3a6483f11d5a7a47199
Fatih Baser <basese_aehit@students.vizja.pl>
2025-01-06 23:26:31 +0100

    Koin added

---
+++ app/src/main/java/com/fatihbaser/cryptotrackerapp/CryptoTrackerApp.kt
@@ -0,0 +1,19 @@
+package com.fatihbaser.cryptotrackerapp
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
+
+class CryptoTrackerApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@CryptoTrackerApp)
+            androidLogger()
+
+            modules(appModule)
+        }
+    }
+}
---
+++ app/src/main/java/com/fatihbaser/cryptotrackerapp/di/AppModule.kt
@@ -0,0 +1,14 @@
+package com.fatihbaser.cryptotrackerapp.di
+
+import io.ktor.client.engine.cio.CIO
+import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+val appModule = module {
+    single { HttpClientFactory.create(CIO.create()) }
+    singleOf(::RemoteCoinDataSource).bind<CoinDataSource>()
+
+    viewModelOf(::CoinListViewModel)
+}

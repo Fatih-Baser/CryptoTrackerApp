fe62754d2f2c42531f26b947ced869e656a9488d
Fatih Baser <basese_aehit@students.vizja.pl>
2025-01-14 20:36:00 +0100

    con list screen added

--- app/src/main/java/com/fatihbaser/cryptotrackerapp/MainActivity.kt
+++ app/src/main/java/com/fatihbaser/cryptotrackerapp/MainActivity.kt
@@ -8,9 +8,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.fatihbaser.cryptotrackerapp.core.data.presentation.util.ObserveAsEvents
 import com.fatihbaser.cryptotrackerapp.crypto.presentation.coin_list.CoinDetailScreen
@@ -50,9 +53,9 @@ class MainActivity : ComponentActivity() {
                         }
                         else -> {
                             CoinListScreen(
-                             //   state = state,
+                               state = state,
                                 modifier = Modifier.padding(innerPadding),
-                               // onAction = viewModel::onAction
+                                onAction = viewModel::onAction
                             )
                         }
                     }
